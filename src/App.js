4a5b6cd8e7425766cb47c25cffc48e0f2f7d84f8
Abdul Haqani <ahaqa011@uottawa.ca>
2020-01-02 17:14:20 -0500

    small commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1> test </h1>
+        <Navbar></Navbar>
       </div>
     );
   }
