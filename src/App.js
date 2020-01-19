2c4ce234a6e6e31bea67fe05a27ca410e8af3f07
Abdul Haqani <ahaqa011@uottawa.ca>
2020-01-18 20:09:36 -0500

    configured api pull

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="app">
         <Navbar></Navbar>
         <div className="container">
-          <Users></Users>
+          <Users loading={this.state.loading} users={this.state.users}></Users>
         </div>
       </div>
     );
